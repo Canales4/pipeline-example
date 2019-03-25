f1e6624ff414d035f8a6849f87b8b46eebf47f40
Juan Carlos Canales <canaleslinaresjuancarlos@gmail.com>
2019-03-25 13:21:19 +0100

    prueba

---
+++ src/test/java/AppFunctionalTest.java
@@ -0,0 +1,24 @@
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+public class AppFunctionalTest {
+	@Rule
+	public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+			.withDesiredCapabilities(DesiredCapabilities.chrome());
+
+	@Test
+	public void tittlePageOk() {
+		RemoteWebDriver driver = chrome.getWebDriver();
+
+		driver.get("http://" + DockerClientFactory.instance().dockerHostIpAddress() + ":8888/laboratorio/");
+		assertEquals("Laboratorio", driver.getTitle());
+	}
+}
