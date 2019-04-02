acf0b596dfd8c9de36af68e10e22c78ebdf9c46a
6001290 <canaleslinaresjuancarlos@gmail.com>
2019-04-02 10:48:38 +0200

    cambio en los test

--- src/test/java/laboratorio/AppFunctionalTest.java
+++ src/test/java/laboratorio/AppFunctionalTest.java
@@ -1,22 +1,16 @@
 package laboratorio;
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Rule;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testcontainers.DockerClientFactory;
-import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AppFunctionalTest {
-	@Rule
-	public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
-			.withDesiredCapabilities(DesiredCapabilities.chrome());
 
 	@Test
 	public void tittlePageOk() {
-		RemoteWebDriver driver = chrome.getWebDriver();
-		driver.get("http://" + DockerClientFactory.instance().dockerHostIpAddress() + ":8888/laboratorio/");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://localhost:8081/laboratorio");
 		assertEquals("Laboratorio", driver.getTitle());
 	}
 }
