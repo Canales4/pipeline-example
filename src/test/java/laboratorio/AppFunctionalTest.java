9156ec6503bf940d32f9071cfea0ccc2ced9e842
6001290 <canaleslinaresjuancarlos@gmail.com>
2019-04-03 10:31:43 +0200

    cambios en los test funcionales

--- src/test/java/laboratorio/AppFunctionalTest.java
+++ src/test/java/laboratorio/AppFunctionalTest.java
@@ -1,15 +1,14 @@
 package laboratorio;
 
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 
 
 public class AppFunctionalTest {
 
 	@Test
-	public void tittlePageOk() throws IOException{
-		Process proc = Runtime.getRuntime().exec("java -jar C:\\Users\\6001290\\Downloads\\selenium-server-standalone-3.141.59.jar -enablePassThrough false");
+	public void tittlePageOk() {
+
 	}
 }
