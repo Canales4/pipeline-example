06c1873832b4bf81b72bb81e497acfc4d874c8b1
6001290 <canaleslinaresjuancarlos@gmail.com>
2019-04-03 09:34:14 +0200

    cambios en el pipeline

--- src/test/java/laboratorio/AppFunctionalTest.java
+++ src/test/java/laboratorio/AppFunctionalTest.java
@@ -13,5 +13,7 @@ public class AppFunctionalTest {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://localhost:8081/laboratorio");
 		System.out.print(driver.getTitle());
+		Desktop d = Desktop.getDesktop();
+		d.browse(new URI("http://localhost:8081/laboratorio"));
 	}
 }
