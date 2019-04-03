dec3f545be00eb6ed216c9c76f605357fcf06471
6001290 <canaleslinaresjuancarlos@gmail.com>
2019-04-03 10:46:09 +0200

    cambios en los test funcionales

--- src/test/java/laboratorio/AppFunctionalTest.java
+++ src/test/java/laboratorio/AppFunctionalTest.java
@@ -1,14 +1,17 @@
 package laboratorio;
 
 import org.junit.Test;
-
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class AppFunctionalTest {
 
 	@Test
 	public void tittlePageOk() {
-
+        String exePath = "C:\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exePath);
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8081/laboratorio");
 	}
 }
